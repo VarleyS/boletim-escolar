7dc549012ce43636677beff41d044db1130aa387
VarleyS <varleysr@gmail.com>
2024-03-04 16:11:04 -0300

    Outras formas de adicionar componentes

--- src/App.js
+++ src/App.js
@@ -14,11 +14,26 @@ class App extends React.Component {
     });
   }
 
+  criaComboBox = () => {
+    const opcoes = ["pessoa1", "pessoa2"]
+    const comboBoxOpcoes = opcoes.map(opcoes => <option>{opcoes}</option>)
+
+    return (
+      <select>
+        {comboBoxOpcoes}
+      </select>
+    )
+  }
+
   render() {
+
+    const MeuComboBox = () => this.criaComboBox()
+
     return (
       <>
         <input type="text" value={this.state.nome} onChange={ this.modificarNome }/><br/>
         <h1>Hello {this.state.nome}!</h1>
+        <MeuComboBox />
       </>
     )
   }
