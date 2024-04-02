93d92e969f41ea3711d000933c59ab95410a940b
VarleyS <varleysr@gmail.com>
2024-04-02 06:12:57 -0300

    "Added form fields to CadastroAluno component and updated Home component with card layout"
    This commit includes changes to the `src/views/alunos/cadastro.js` file where form fields have been added to the `CadastroAluno` component, and the `state` has been initialized to manage the form field values. Additionally, the `Home` component in the `src/views/home.js` file has been updated with a card layout.

--- src/views/alunos/cadastro.js
+++ src/views/alunos/cadastro.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class CadastroAluno extends React.Component {
+
+    state = {
+        nome: '',
+        dataNascimento: 01/01/2000, // Formato DD/MM/AAAA
+        cpf: '',
+        rg: '',
+        sexo: '',
+        telefone: ''
+    }
+
     render() {
         return (
             <div className="card">
@@ -8,24 +18,24 @@ class CadastroAluno extends React.Component {
                     <h2>Cadastro de Alunos</h2>
                 </div>
                 <div className="card-body">
-                    <div className="img-aluno">
+                    {/* <div className="img-aluno">
                         <label htmlFor="imagem">
                             <h5>Adicionar Imagem do Aluno:</h5>
                             <input type="file" id="imagem"/>
                         </label>
-                    </div>
+                    </div> */}
                     <div className="row">
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Nome: *</label>
-                                <input type="text" className="form-control" />  
+                                <input type="text" className="form-control" />
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Data de Nascimento: *</label>
-                                <input type="text" className="form-control" />  
+                                <input type="text" className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -33,14 +43,14 @@ class CadastroAluno extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>CPF: </label>
-                                <input type="text" className="form-control" />  
+                                <input type="text" className="form-control" />
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>RG: *</label>
-                                <input type="text" className="form-control" />  
+                                <input type="text" className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -48,21 +58,27 @@ class CadastroAluno extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Sexo: *</label>
-                                <input type="text" className="form-control" />  
+                                <input type="text" className="form-control" />
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Telefone Responsável: *</label>
-                                <input type="text" className="form-control" />  
+                                <input type="text" className="form-control" />
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="btn">
-                <button type="button" class="btn btn-light">Salvar</button>
-                <button type="button" class="btn btn-light">Cancelar</button>
+
+                    <div className="row">
+                        <div className="col-md-1">
+                            <button className="btn btn-success" >Salvar</button>
+                        </div>
+
+                        <div className="col-md-1">
+                            <button className="btn btn-primary" >Cancelar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/views/home.js
+++ src/views/home.js
@@ -3,24 +3,26 @@ import React from "react";
 function Home() {
     return (
         <div className="container">
-            <div>
-                <h2>Example body text</h2>
-                <p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
-                <p><small>This line of text is meant to be treated as fine print.</small></p>
-                <p>The following is <strong>rendered as bold text</strong>.</p>
-                <p>The following is <em>rendered as italicized text</em>.</p>
-                <p>An abbreviation of the word attribute is <abbr title="attribute">attr</abbr>.</p>
+            <div className="card">
+                <div className="card-header" >
+                    <h2>Example body text</h2>
+                    <p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
+                    <p><small>This line of text is meant to be treated as fine print.</small></p>
+                    <p>The following is <strong>rendered as bold text</strong>.</p>
+                    <p>The following is <em>rendered as italicized text</em>.</p>
+                    <p>An abbreviation of the word attribute is <abbr title="attribute">attr</abbr>.</p>
+                
+                    <figure>
+                        <blockquote class="blockquote">
+                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+
+                        </blockquote>
+                        <figcaption class="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
-
-            <figure>
-                <blockquote class="blockquote">
-                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-
-                </blockquote>
-                <figcaption class="blockquote-footer">
-                    Someone famous in <cite title="Source Title">Source Title</cite>
-                </figcaption>
-            </figure>
         </div>
     )
 }
