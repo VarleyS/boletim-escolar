d071c8d00726cb6884cd2402818838685b599baf
VarleyS <varleysr@gmail.com>
2024-04-02 18:07:10 -0300

    "Added state management and event handlers to CadastroAluno component"
    The code diff shows the addition of state management and event handlers to the CadastroAluno component. Specifically, a constructor method has been added to initialize the state object, and two new methods, `onChange` and `onSubmit`, have been added to handle changes to form inputs and submit the form data, respectively. The `onChange` method is used to update the state based on user input, and the `onSubmit` method is used to log the state to the console when the form is submitted. Additionally, the input fields for the form have been updated to use the `onChange` method and to initialize their values with the corresponding state properties.

--- src/views/alunos/cadastro.js
+++ src/views/alunos/cadastro.js
@@ -4,11 +4,21 @@ class CadastroAluno extends React.Component {
 
     state = {
         nome: '',
-        dataNascimento: 01/01/2000, // Formato DD/MM/AAAA
+        dataNascimento: '01/01/2000', // Formato DD/MM/AAAA
         cpf: '',
         rg: '',
         sexo: '',
         telefone: ''
+    }
+
+    onChange = (event) => {
+        const valor = event.target.value;
+        const nomeCampo = event.target.name;
+        this.setState({[nomeCampo]: valor});        
+    }
+
+    onSubmit = (event) => {
+        console.log(this.state)
     }
 
     render() {
@@ -28,14 +38,24 @@ class CadastroAluno extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Nome: *</label>
-                                <input type="text" className="form-control" />
+                                <input 
+                                type="text" 
+                                name="nome" 
+                                onChange={this.onChange} 
+                                value={this.state.nome} 
+                                className="form-control" />
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Data de Nascimento: *</label>
-                                <input type="text" className="form-control" />
+                                <input 
+                                type="text" 
+                                name="dataNascimento" 
+                                onChange={this.onChange} 
+                                value={this.state.dataNascimento} 
+                                className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -43,14 +63,24 @@ class CadastroAluno extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>CPF: </label>
-                                <input type="text" className="form-control" />
+                                <input 
+                                type="text" 
+                                name="cpf" 
+                                onChange={this.onChange} 
+                                value={this.state.cpf} 
+                                className="form-control" />
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>RG: *</label>
-                                <input type="text" className="form-control" />
+                                <input 
+                                type="text" 
+                                name="rg" 
+                                onChange={this.onChange} 
+                                value={this.state.rg} 
+                                className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -58,21 +88,31 @@ class CadastroAluno extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Sexo: *</label>
-                                <input type="text" className="form-control" />
+                                <input 
+                                type="text" 
+                                name="sexo" 
+                                onChange={this.onChange} 
+                                value={this.state.sexo} 
+                                className="form-control" />
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Telefone Responsável: *</label>
-                                <input type="text" className="form-control" />
+                                <input 
+                                type="text" 
+                                name="telefone" 
+                                onChange={this.onChange} 
+                                value={this.state.telefone} 
+                                className="form-control" />
                             </div>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-md-1">
-                            <button className="btn btn-success" >Salvar</button>
+                            <button onClick={this.onSubmit} className="btn btn-success" >Salvar</button>
                         </div>
 
                         <div className="col-md-1">
